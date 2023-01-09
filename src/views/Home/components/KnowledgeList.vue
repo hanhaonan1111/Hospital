0eb869edf523f51ecc3a1ed0bd57c922fd5ed4b7
hanhaonan1111 <hhnboy@163.com>
2023-01-09 19:00:45 +0800

    首页搭建->OK

---
+++ src/views/Home/components/KnowledgeList.vue
@@ -0,0 +1,17 @@
+<template>
+  <KnowledgeCard
+    v-for="(v, i) in 5"
+    :key="i"
+    class="knowledge-list"
+  ></KnowledgeCard>
+</template>
+
+<script setup lang="ts">
+import KnowledgeCard from "./KnowledgeCard.vue";
+</script>
+
+<style scoped lang="scss">
+.knowledge-list {
+  padding: 5px 15px 51px;
+}
+</style>
